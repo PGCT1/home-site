7915cd560afca11f07e2c9e33645fca32af36a84
PGCT1 <t1gemini@yandex.ru>
2014-05-26 02:08:08 +0400

    init

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-home-site
-=========
+# Home Site for Project Gemini
+
+### Frameworks/Engines
+
+ - [Sails](https://github.com/balderdashy/sails)
+ - [Jade](https://github.com/visionmedia/jade)
+ - [Less](https://github.com/less/less.js)
+ - [Angular](https://github.com/angular/angular.js)
+
+### Modules
+
+ - [Angular Carousel](https://github.com/revolunet/angular-carousel)
---
+++ assets/js/index.js
@@ -0,0 +1,62 @@
+
+(function(){
+
+	var main = angular.module('main',['angular-carousel']);
+
+	main.controller('IPadController',['$scope',function($scope){
+
+    var slideCount = 4;
+    var duration = 15000;
+
+		this.index = 0;
+    this.expectedNext = 1;
+
+    //we try to predict the next slide; if we predict incorrectly, they've
+    //manually changed slides, so we do nothing that iteration. this means if the 
+    //use manually switches to a slide, they will always get at least the full duration
+    //of a cycle to read it
+
+    this.setNext = function(){
+
+      var originalValue = this.index;
+
+      if(this.index < (slideCount - 1)){
+        this.index++;
+      }else{
+        this.index = 0;
+      }
+
+      if(this.index == this.expectedNext){
+        $scope.$apply();
+      }else{
+        this.index = originalValue;
+      }
+
+      if(this.index == (slideCount - 1)){
+        this.expectedNext = 0;
+      }else{
+        this.expectedNext = this.index + 1;
+      }
+
+    }
+
+    setInterval(function(){
+      this.setNext();
+    }.bind(this),duration);
+
+	}]);
+
+})();
+
+$().ready(function(){
+
+  //set email to contact button href
+  
+  var base64 = 'eW91ckBlbWFpbC5jb20=';
+
+  setTimeout(function(){
+    $('#con'+'tact').attr('href','mail'+'to:'+atob(base64));
+  },1000);
+
+})
+
